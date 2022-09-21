610924c68af8c4fb9f7de3bdd119b2b9332b3838
yukinissie <bigtree2490@outlook.jp>
2022-09-21 16:35:05 +0900

    Add GamePlayComponent

--- src/components/game_play.rs
+++ src/components/game_play.rs
@@ -3,14 +3,15 @@ use yew::prelude::*;
 #[function_component(GamePlay)]
 pub fn game_play() -> Html {
     html! {
-        <section>
-            <h1>{"さっそくプレイ！（ブラウザ版）"}</h1>
+        <section class="flex flex-col w-full">
+            <h1 class="text-center text-6xl mt-16">{"さっそくプレイ！（ブラウザ版）"}</h1>
             <iframe
                 id="Jyogi Games"
                 title="Jyogi Games"
                 width="960"
                 height="600"
-                src="https://jyogi-games.yukinissie.com">
+                src="https://jyogi-games.yukinissie.com"
+                class="mx-auto mt-16 mb-16">
             </iframe>
         </section>
     }
