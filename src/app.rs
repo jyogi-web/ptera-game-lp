46257467d750a3222e0f0b915e8464ee5930517f
yukinissie <bigtree2490@outlook.jp>
2022-09-21 18:51:59 +0900

    cargo fmt

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,6 @@
+use crate::components::about_tech::*;
 use crate::components::game_play::*;
 use crate::components::top::*;
-use crate::components::about_tech::*;
 use yew::prelude::*;
 
 #[function_component(App)]
