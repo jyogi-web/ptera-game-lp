cabb65684736ab568f3bef9717f28082bd2b0089
yukinissie <bigtree2490@outlook.jp>
2022-09-22 01:26:48 +0900

    updated top background image

--- src/components/top.rs
+++ src/components/top.rs
@@ -6,11 +6,13 @@ pub fn top() -> Html {
         <section>
             <div class="bg-[url('static/BackGroundImage.svg')] bg-center w-full h-screen bg-cover absolute"/>
             <div class="relative h-screen w-32">
-                <h1 class="absolute text-9xl bottom-3 left-3" style="background: linear-gradient(117.17deg, #FF0000 0%, #2400FF 97.74%);
+                <h1 class="absolute text-9xl" style="background: linear-gradient(117.17deg, #FF0000 0%, #2400FF 97.74%);
                 -webkit-background-clip: text;
                 -webkit-text-fill-color: transparent;
                 background-clip: text;
-                text-fill-color: transparent;">{"Ready\nEliminate\nDominator"}</h1>
+                text-fill-color: transparent;
+                left:80px;
+                bottom:100px">{"Ready\nEliminate\nDominator"}</h1>
             </div>
         </section>
     }
