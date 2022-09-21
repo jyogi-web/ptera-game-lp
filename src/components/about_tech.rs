1e19fb6c8521176c8b355c876398891d227c3b91
yukinissie <bigtree2490@outlook.jp>
2022-09-21 22:50:09 +0900

    updated topa'z link

--- src/components/about_tech.rs
+++ src/components/about_tech.rs
@@ -5,7 +5,7 @@ pub fn about_tech() -> Html {
     html! {
         <section class="flex flex-col">
         <h1 class="text-center text-6xl mt-16">{"技術的な詳細や開発背景はこちら！"}</h1>
-        <a href="https://topaz.dev" class="relative mx-auto mt-16 mb-16" style="background: linear-gradient(90deg, #28A2BF 0%, #4CC6BB 100%);
+        <a href="https://topaz.dev/projects/0bdca801952a9a59bba7" class="relative mx-auto mt-16 mb-16" style="background: linear-gradient(90deg, #28A2BF 0%, #4CC6BB 100%);
             border-radius: 25px;
             width: 645px;
             height: 140px;">
