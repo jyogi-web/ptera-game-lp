8afe8add49c697a400c876c96f157c56de44e71f
yukinissie <bigtree2490@outlook.jp>
2022-09-22 12:41:42 +0900

    文言追加

--- src/components/game_play.rs
+++ src/components/game_play.rs
@@ -5,6 +5,7 @@ pub fn game_play() -> Html {
     html! {
         <section class="flex flex-col w-full">
             <h1 class="text-center text-6xl mt-16">{"さっそくプレイ！（ブラウザ版）"}</h1>
+            <p class="text-center text-xl mt-16">{"黒い画面のときはロード中です。ダウンロード版ならすぐに遊べます。"}</p>
             <iframe
                 id="RED"
                 title="RED"
